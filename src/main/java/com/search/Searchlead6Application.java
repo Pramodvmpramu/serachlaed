a06453f0ab8c3a063a1c0c3b3b09c914286bac30
pramodvmpraamu <pramodvmpramu@gmail.com>
2023-10-01 17:30:04 +0530

    searchlead backup

---
+++ src/main/java/com/search/Searchlead6Application.java
@@ -0,0 +1,13 @@
+package com.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Searchlead6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Searchlead6Application.class, args);
+	}
+
+}
